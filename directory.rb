f147174de69f2c5031bbc3373025792589eed53b
Lisa <bardelli.lisa@gmail.com>
2020-11-17 17:08:17 +0000

    step8_exercise5 ask for hobbies and country of birth

--- directory.rb
+++ directory.rb
@@ -1,36 +1,43 @@
 def print_header
-  puts 'The students of Villains Academy'
+  puts 'The students of Makers Academy'
   puts '-------------'
 end
+
+
 def input_students
+    students = []
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    students = []
     name = gets.chomp
+
     while !name.empty? do
-      students << {name: name, cohort: :november}
+        puts 'Please enter his/her hobbies'
+        hobbies = gets
+        puts 'Please enter his/her country of birth'
+        country = gets
+        students << { name: name, cohort: :november, hobbies: hobbies.chomp, country_of_birth: country.chomp }
       puts "Now we have #{students.count} students"
       name = gets.chomp
     end
     students
   end
 
-  def print(students)
-    count_student = 0
-    tot_student = students.count
-    while count_student < tot_student
-      (0...students.length).each do |i|
-        student = students[i]
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-        count_student += 1
-      end
+def print(students)
+  count_student = 0
+  tot_student = students.count
+  while count_student < tot_student
+    (0...students.length).each do |i|
+      student = students[i]
+      puts "#{student[:name]} (#{student[:cohort]} cohort). He/she likes #{student[:hobbies]} and is from #{student[:country_of_birth]}."
+      count_student += 1
     end
-    end
-
-  def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
   end
-  students = input_students
-  print_header
-  print(students)
-  print_footer(students)
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+students = input_students
+print_header
+print(students)
+print_footer(students)
