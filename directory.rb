9dd97d0d35cc38db6d3b75e6947dedcd96bb4e3d
Lisa <bardelli.lisa@gmail.com>
2020-11-17 16:06:18 +0000

    Ask for user input

--- directory.rb
+++ directory.rb
@@ -15,6 +15,18 @@ def print_header
   puts 'The students of Villains Academy'
   puts '-------------'
 end
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      name = gets.chomp
+    end
+    students
+  end
 
 def print(students)
     students.each do |student|
@@ -25,6 +37,7 @@ def print(students)
   def print_footer(students)
     puts "Overall, we have #{students.count} great students"
   end
-print_header
-print(students)
-print_footer(students)
+  students = input_students
+  print_header
+  print(students)
+  print_footer(students)
